05d68ac973cea129a28d062e905c9a95ad2314f6
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-09-11 13:06:51 +0200

    feat: add lambda with endpoint /api/test

--- aws_cdk_lambda_sqs/aws_cdk_lambda_sqs_stack.py
+++ aws_cdk_lambda_sqs/aws_cdk_lambda_sqs_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    CfnOutput,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway
 )
 from constructs import Construct
 
@@ -10,10 +11,37 @@ class AwsCdkLambdaSqsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Define the Lambda function
+        hello_lambda = _lambda.Function(
+            self, 'HelloLambda',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler='index.handler',
+            code=_lambda.Code.from_inline(
+                """
+                def handler(event, context):
+                    return {
+                        'statusCode': 200,
+                        'body': '{"message": "Hello, World!"}'
+                    }
+                """
+            )
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkLambdaSqsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the API Gateway
+        api = apigateway.RestApi(
+            self, 'Endpoint',
+            default_cors_preflight_options={
+                'allow_origins': apigateway.Cors.ALL_ORIGINS,
+                'allow_methods': apigateway.Cors.ALL_METHODS
+            }
+        )
+
+        # Add the "/api/test" endpoint and link it to the Lambda function
+        lambda_integration = apigateway.LambdaIntegration(hello_lambda)
+        api_resource = api.root.add_resource('api').add_resource('test')
+        api_resource.add_method('GET', lambda_integration)
+
+        # Print the API endpoint URL
+        CfnOutput(self, "EndpointURL",
+                  value=api.url,
+                  description="The URL of the API Gateway endpoint")
